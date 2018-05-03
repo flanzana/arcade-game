904b7560a04c46cafa415003e029358e9c7e6684
Zana Flander <zana.flander@gmail.com>
2018-05-03 17:55:47 +0200

    initial location added for player and enemies

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
+// ---------------------- E N E M I E S --------------------------
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    //setting enemies initial location
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +19,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,17 +28,47 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+// ---------------------- P L A Y E R --------------------------
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+    this.sprite = 'images/char-boy.png';
+
+    //setting player initial location
+    this.x = x;
+    this.y = y;
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+
+};
 
 
+// ---------------------- INSTANTIATE OBJECTS --------------------------
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var enemy1 = new Enemy(200, 55);
+var enemy2 = new Enemy(-100, 55);
+var enemy3 = new Enemy(100, 140);
+var enemy4 = new Enemy(400, 225);
+
+var allEnemies = [enemy1, enemy2, enemy3, enemy4];
+
+var player = new Player(200, 400);
 
 
-
+// ---------------------- KEYS (DO NOT EDIT!) --------------------------
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
